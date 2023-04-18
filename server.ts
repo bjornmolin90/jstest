27aaa0d6ce282a5628ec2c82ee79efb71ea66382
Björn Molin <bjmo21@student.bth.se>
2023-04-18 14:10:16 +0100

    okej

--- server.ts
+++ server.ts
@@ -3,11 +3,11 @@ import small from "../../data/small.json" assert { type: 'json' };
 import medium from "../../data/medium.json" assert { type: 'json' };
 import big from "../../data/big.json" assert { type: 'json' };
 
-const handler = async (req: Request): Promise<Response> => {
+const handler = async (req) => {
   const url = new URL(req.url);
 
   if (url.pathname  === "/small") {
-    return new Response(JSON.stringify(medium), {
+    return new Response(JSON.stringify(small), {
       status: 200,
       headers: {
         "content-type": "application/json",
